0348db035fb3c6b6a335450bbaadca261026c09c
AngryPowman <wenchaoli@tuputech.com>
2017-11-16 01:29:45 +0800

    update github response

--- src/github-oauth.ts
+++ src/github-oauth.ts
@@ -1,4 +1,6 @@
-import * as r from 'request';
+// import * as r from 'request';
+import * as r from 'request-promise';
+
 import * as url from 'url';
 
 let request = r.defaults({
@@ -9,13 +11,55 @@ let request = r.defaults({
 
 export class GithubOAuth {
     private static readonly BASE_URL = 'https://api.github.com';
-    public static getUser(username: string) {
-        return new Promise((resolve, reject) => {
-            request.get(this.url(`/users/${username}`), {}, (error, response, body) => {
-                console.log(body);
-                resolve(body);
-            });
+    public static async getResume(username: string) {
+        let resume: any = {};
+
+        let [userRsp, reposRsp, orgsRsp, issuesRsp] = await Promise.all([
+            await request.get(this.url(`/users/${username}`)),
+            await request.get(this.url(`/search/repositories?q=user:${username}&sort=stars&order=desc`)),
+            await request.get(this.url(`/users/${username}/orgs`)),
+            await request.get(this.url(`/search/issues?q=type:pr+is:merged+author:${username}&per_page=50`)),
+        ]);
+
+        let user = JSON.parse(userRsp);
+        let repos = JSON.parse(reposRsp);
+        let orgs = JSON.parse(orgsRsp);
+        let issues = JSON.parse(issuesRsp);
+        
+        resume.user = {
+            "name": user.name,
+            "avatar_url": user.avatar_url,
+            "company": user.company,
+            "website": user.website,
+            "location": user.location,
+            "email": user.email,
+            "hireable": user.hireable,
+            "bio": user.bio,
+            "public_repos": user.public_repos,
+            "contributed_repos": issues.total_count,
+            "followers": user.followers,
+            "following": user.following,
+            "orgs_count": orgs.length,
+            "created_at": user.created_at
+        };
+
+        resume.repos = [];
+        repos.items.forEach(repo => {
+            let repoInfo = {
+                "name": repo.name,
+                "stars": repo.stargazers_count,
+                "forks": repo.forks,
+                "watchers": repo.watchers,
+                "description": repo.description,
+                "created_at": repo.created_at,
+                "language": repo.language,
+                "url": repo.html_url,
+            };
+
+            resume.repos.push(repoInfo);
         });
+
+        return resume;
     }
 
     private static url(to: string) {
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ const LISTEN_PORT = 23333;
 let app = express();
 
 app.get('/resume/:username', async (req, res) => {
-    let response = await GithubOAuth.getUser(req.params.username);
+    res.set('Content-Type', 'application/json');
+    res.set('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    
+    let response = await GithubOAuth.getResume(req.params.username);
     res.send(response);
 });
 
 app.get('/', (req, res) => {
-    res.send('fuckoff resume');
+    res.send('kingwl NO.1');
 });
 
 app.listen(LISTEN_PORT, () => {
